b689819b929e99989aed4387f44c389461bc2796
Morgan Strong <mostro20@gmail.com>
2021-06-02 12:56:44 +1000

    Fix join in temporary tables

--- Version2/CMS-to-DAMS_export/combine-caption-data_v5.sql
+++ Version2/CMS-to-DAMS_export/combine-caption-data_v5.sql
@@ -3,8 +3,8 @@ DROP PROCEDURE IF EXISTS ROWPERROW5;
 DELIMITER //
 CREATE PROCEDURE ROWPERROW5()
 BEGIN
-    TRUNCATE caption_data3;
-    INSERT INTO caption_data3
+    TRUNCATE caption_data;
+    INSERT INTO caption_data
     (
         cms_irn,
         object_type,
@@ -24,50 +24,52 @@ BEGIN
     )
     WITH
         epartiesinfo AS (
-            SELECT
+            SELECT DISTINCT
                 NULLIF(cre.PartiesSummaryData, '') AS PartiesSummaryData,
                 NULLIF(cre.BioBirthDate, '') AS BioBirthDate,
                 NULLIF(cre.BioDeathDate, '') AS BioDeathDate,
                 NULLIF(cre.BioCulturalIdentity1, '') AS BioCulturalIdentity1,
                 NULLIF(cre.Country, '') AS Country,
-                NULLIF(cre.PartiesIRN, '') AS PartiesIRN
+                NULLIF(cre.PartiesIRN, '') AS PartiesIRN,
+                objirn.ObjectIRN AS ObjectIRN
             FROM Objects_attachedCreatorIRNS_csv objirn
                 INNER JOIN eparties cre ON objirn.CreatorIRN = cre.PartiesIRN
         ),
         erightsinfo AS (
-            SELECT
+            SELECT DISTINCT
                 r.RigAcknowledgement,
-                r.irn
+                r.irn,
+                rigirn.ObjectIRN AS ObjectIRN
             FROM Objects_attachedRightsIRNS_csv rigirn
                 INNER JOIN erights r ON rigirn.RightsIRN = r.irn
 
             WHERE r.RigAcknowledgement IS NOT NULL AND r.RigAcknowledgement != ''
         )
-    SELECT
+    SELECT distinct
         e.irn AS cms_irn,
         e.TitObjectStatus AS object_type,
         e.TitAccessionNo AS accession_no,
         e.TitMainTitle AS title,
         e.CreDateCreated AS 'year',
-        (SELECT group_concat(concat_ws(' ', ri1.RigAcknowledgement) order by ri1.irn separator '\n') FROM erightsinfo ri1 WHERE ri1.irn = e.irn) AS rights,
-        (SELECT group_concat(concat_ws(' ', pi1.PartiesSummaryData, ' b.', pi1.BioBirthDate, ' - ', pi1.BioDeathDate) order by pi1.PartiesIRN separator '\n') FROM epartiesinfo pi1 WHERE pi1.PartiesIRN = e.irn) AS creators,
+        (SELECT group_concat(concat_ws(' ', ri1.RigAcknowledgement) order by ri1.irn separator '\n') FROM erightsinfo ri1 WHERE ri1.ObjectIRN = e.irn) AS rights,
+        (SELECT group_concat(concat_ws(' ', pi1.PartiesSummaryData, ' b.', pi1.BioBirthDate, ' - ', pi1.BioDeathDate) order by pi1.PartiesIRN separator '\n') FROM epartiesinfo pi1 WHERE pi1.ObjectIRN = e.irn) AS creators,
         'place_created' AS place_created,
         CONCAT(e.PhyVerbatimMeasurementsA, ' ', e.PhyVerbatimMeasurementsB) AS dimensions,
         e.AccCreditLineLocal AS credit,
         CONCAT(e.PhyMediumText, ' ', e.PhySupport) AS media_statement,
         'digital fields' AS special_digital_meta,
-        (SELECT group_concat(concat_ws(' ', e.TitMainTitle, ' ', e.CreDateCreated, ' ', pi2.PartiesSummaryData, ' b.', pi2.BioBirthDate, ' - ', pi2.BioDeathDate) order by pi2.PartiesIRN separator '\n') FROM epartiesinfo pi2 WHERE pi2.PartiesIRN = e.irn) AS short_caption,
+        (SELECT group_concat(concat_ws(' ', e.TitMainTitle, ' ', e.CreDateCreated, ' ', pi2.PartiesSummaryData, ' b.', pi2.BioBirthDate, ' - ', pi2.BioDeathDate) order by pi2.PartiesIRN separator '\n') FROM epartiesinfo pi2 WHERE pi2.ObjectIRN = e.irn) AS short_caption,
         CONCAT_WS(' ',
             (SELECT group_concat(concat_ws(' ', pi3.PartiesSummaryData, CHAR(13),NULLIF(pi3.BioCulturalIdentity1, ''),pi3.Country, ' ',  ' b.', pi3.BioBirthDate, ' - ', pi3.BioDeathDate) order by pi3.PartiesIRN separator '\n')
             FROM epartiesinfo pi3
-            WHERE pi3.PartiesIRN = e.irn),
+            WHERE pi3.ObjectIRN = e.irn),
                 NULLIF(e.TitMainTitle, ''), ' ', NULLIF(e.CreDateCreated, ''), CHAR(13),
                 NULLIF(e.PhyMediumText, ''), ' ', NULLIF(e.PhySupport, ''), CHAR(13),
                 NULLIF(e.PhyVerbatimMeasurementsA, ''), ' ', NULLIF(e.PhyVerbatimMeasurementsB, ''), CHAR(13),
                 'Acc.', NULLIF(e.TitAccessionNo, ''), CHAR(13),
                 'Temp no.', NULLIF(e.TitPreviousNumber, ''), CHAR(13),
                 NULLIF(e.AccCreditLineLocal, ''), CHAR(13),
-                (SELECT group_concat(concat_ws(' ', NULLIF(ri2.RigAcknowledgement, ''))order by ri2.irn separator '\n') FROM erightsinfo ri2 WHERE ri2.irn = e.irn)
+                (SELECT group_concat(concat_ws(' ', NULLIF(ri2.RigAcknowledgement, ''))order by ri2.irn separator '\n') FROM erightsinfo ri2 WHERE ri2.ObjectIRN = e.irn)
             ) AS long_caption,
         DATE(NOW()) AS last_updated
     FROM ecatalogue e
