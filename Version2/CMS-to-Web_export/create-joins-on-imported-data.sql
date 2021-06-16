8f2743f35a6fd94d6caf194aded83252baa24cc7
Morgan Strong <mostro20@gmail.com>
2021-06-16 10:04:21 +1000

    Update SQL statements to include export statement

--- Version2/CMS-to-Web_export/create-joins-on-imported-data.sql
+++ Version2/CMS-to-Web_export/create-joins-on-imported-data.sql
@@ -4,23 +4,23 @@
 /*Import the CSVs as mapping tables*/
 
 /*Initially alter the ecatalogue table and add an image URL - Sub Out DB name*/
-ALTER TABLE `20210526_Web`.ecatalogue ADD ImageURL varchar(255) NULL;
-ALTER TABLE `20210526_Web`.ecatalogue ADD DetRights varchar(255) NULL;
-ALTER TABLE `20210526_Web`.eparties ADD ObjectRef TEXT NULL;
-ALTER TABLE `20210526_Web`.enarratives ADD ObjectRef TEXT NULL;
-ALTER TABLE `20210526_Web`.enarratives ADD PartiesRef TEXT NULL;
+ALTER TABLE `20210615_Web`.ecatalogue ADD ImageURL varchar(255) NULL;
+ALTER TABLE `20210615_Web`.ecatalogue ADD DetRights varchar(255) NULL;
+ALTER TABLE `20210615_Web`.eparties ADD ObjectRef TEXT NULL;
+ALTER TABLE `20210615_Web`.enarratives ADD ObjectRef TEXT NULL;
+ALTER TABLE `20210615_Web`.enarratives ADD PartiesRef TEXT NULL;
 
 /*Create all necessary indexes*/
-CREATE INDEX emultimedia_irn_IDX USING BTREE ON `20210526_Web`.emultimedia (irn);
-CREATE INDEX ecatalogue_irn_IDX USING BTREE ON `20210526_Web`.ecatalogue (irn);
-CREATE INDEX enarratives_NarrativeIRN_IDX USING BTREE ON `20210526_Web`.enarratives (NarrativeIRN);
-CREATE INDEX eparties_PartiesIRN_IDX USING BTREE ON `20210526_Web`.eparties (PartiesIRN);
-CREATE INDEX Narratives_AttachmentsIRNS_NarrativeIRN_IDX USING BTREE ON `20210526_Web`.Narratives_AttachmentsIRNS (NarrativeIRN,AuthorIRN,ContribIRN,MasterIRN);
-CREATE INDEX Objects_CreatorIRNS_ObjectIRN_IDX USING BTREE ON `20210526_Web`.Objects_CreatorIRNS (ObjectIRN,CreatorIRN);
-CREATE INDEX Objects_MultimediaIRNS_ObjectIRN_IDX USING BTREE ON `20210526_Web`.Objects_MultimediaIRNS (ObjectIRN,MultimediaIRN);
+CREATE INDEX emultimedia_irn_IDX USING BTREE ON `20210615_Web`.emultimedia (irn);
+CREATE INDEX ecatalogue_irn_IDX USING BTREE ON `20210615_Web`.ecatalogue (irn);
+CREATE INDEX enarratives_NarrativeIRN_IDX USING BTREE ON `20210615_Web`.enarratives (NarrativeIRN);
+CREATE INDEX eparties_PartiesIRN_IDX USING BTREE ON `20210615_Web`.eparties (PartiesIRN);
+CREATE INDEX Narratives_AttachmentsIRNS_NarrativeIRN_IDX USING BTREE ON `20210615_Web`.Narratives_AttachmentsIRNS (NarrativeIRN,AuthorIRN,ContribIRN,MasterIRN);
+CREATE INDEX Objects_CreatorIRNS_ObjectIRN_IDX USING BTREE ON `20210615_Web`.Objects_CreatorIRNS (ObjectIRN,CreatorIRN);
+CREATE INDEX Objects_MultimediaIRNS_ObjectIRN_IDX USING BTREE ON `20210615_Web`.Objects_MultimediaIRNS (ObjectIRN,MultimediaIRN);
 
 /*Now add image URL via the Objects_Multimedia Mapping table*/
-USE `20210526_Web`;
+USE `20210615_Web`;
 UPDATE ecatalogue ec
 JOIN Objects_MultimediaIRNS omi ON omi.ObjectIRN = ec.irn
 JOIN emultimedia mm ON mm.irn = omi.MultimediaIRN
@@ -29,7 +29,7 @@ SET ec.ImageURL = Concat('/sites/default/files/', mm.MulIdentifier),
 
 /* Add Objects to Parties */
 SET SESSION group_concat_max_len = 1000000;
-USE `20210526_Web`;
+USE `20210615_Web`;
 UPDATE eparties ep
 SET ep.ObjectRef = (
 	SELECT GROUP_CONCAT(DISTINCT cs.ObjectIRN SEPARATOR ';')
@@ -39,14 +39,14 @@ SET ep.ObjectRef = (
 	)
 
 /* Add objects to narratives - not current export is doing enarratives to eobjects direct, this may need to change*/
-USE `20210526_Web`;
+USE `20210615_Web`;
 UPDATE enarratives en
 JOIN ecatalogue ec ON ec.TitAccessionNo = en.DetNarrativeIdentifier
 SET en.ObjectRef = ec.irn
 
-/* Add authors to narratives */
+/* Add authors to narratives - needs work for join to parties*/
 SET SESSION group_concat_max_len = 1000000;
-USE `20210526_Web`;
+USE `20210615_Web`;
 UPDATE enarratives en
 SET en.PartiesRef = (
 	SELECT GROUP_CONCAT(DISTINCT cs.AuthorIRN SEPARATOR ';')
@@ -54,3 +54,62 @@ SET en.PartiesRef = (
     WHERE en.NarrativeIRN = ep.PartiesIRN
 	GROUP BY cs.NarrativeIRN
 	)
+
+
+/*ONCE FULLY EXECUTED NOW NEED TO QUERY DATA FOR FEEDS INGEST*/
+
+/*Export Objects*/
+SELECT
+irn AS "ecatalogue_key",
+TitAccessionNo AS "Accession No.",
+AccCreditLineLocal AS "Credit Line",
+TitMainTitle AS "Title",
+CrePlaceOfExecution AS "Place of execution",
+PhyMediaCategory AS "Medium",
+PhyVerbatimMeasurementsA AS "Main measurement",
+PhyVerbatimMeasurementsB AS "Secondary measurement",
+CreDateCreated AS "Date Created",
+CreEarliestDate AS "EarliestDate",
+CreLatestDate AS "LatestDate",
+PhySupport AS "Physical support",
+EdiImpression AS "Edition",
+TitDepartment AS "QAGOMA Department",
+PhyMediaCategory AS "Primary Media",
+PhySecondaryMediaCategory AS "Secondary Media",
+ImageURL AS "URL",
+ObjectType AS "Subjects",
+PhyMediumText AS "MediumText",
+DetRights AS "Rights"
+FROM ecatalogue
+WHERE AdmPublishWebPassword LIKE 'Yes'
+
+/*Export Parties*/
+SELECT
+NamFirst,
+NamLast,
+NamOrganisation,
+Country,
+BioBirthPlace,
+BioDeathPlace,
+BioBirthDate,
+BioDeathDate,
+PartiesIRN,
+BioCulturalIdentity1,
+ObjectRef,
+CONCAT(NamFirst, ' ', NamLast, NamOrganisation) AS Title
+FROM eparties
+WHERE PartiesPublishInternet LIKE 'Yes'
+
+/*Export Narratives*/
+SELECT
+NarDate,
+DesPurpose,
+NarTitle,
+DesType,
+DesSubjects,
+NarrativeIRN,
+NarNarrative,
+ObjectRef,
+PartiesRef
+FROM enarratives
+WHERE NarrativePublishInternet LIKE 'Yes'
