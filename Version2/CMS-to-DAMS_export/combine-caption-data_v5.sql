5d6c7fa878904d6cefed683ec30711cb21484095
Morgan Strong <mostro20@gmail.com>
2021-06-25 12:05:40 +1000

    Include role in the exports

--- Version2/CMS-to-DAMS_export/combine-caption-data_v5.sql
+++ Version2/CMS-to-DAMS_export/combine-caption-data_v5.sql
@@ -31,8 +31,9 @@ BEGIN
                 NULLIF(cre.BioCulturalIdentity1, '') AS BioCulturalIdentity1,
                 NULLIF(cre.Country, '') AS Country,
                 NULLIF(cre.PartiesIRN, '') AS PartiesIRN,
+                NULLIF(objirn.CreRole, '') AS CreRole,
                 objirn.ObjectIRN AS ObjectIRN
-            FROM Objects_attachedCreatorIRNS_csv objirn
+            FROM Objects_attachedCreatorIRNS objirn
                 INNER JOIN eparties cre ON objirn.CreatorIRN = cre.PartiesIRN
         ),
         erightsinfo AS (
@@ -40,7 +41,7 @@ BEGIN
                 r.RigAcknowledgement,
                 r.irn,
                 rigirn.ObjectIRN AS ObjectIRN
-            FROM Objects_attachedRightsIRNS_csv rigirn
+            FROM Objects_attachedRightsIRNS rigirn
                 INNER JOIN erights r ON rigirn.RightsIRN = r.irn
 
             WHERE r.RigAcknowledgement IS NOT NULL AND r.RigAcknowledgement != ''
@@ -52,15 +53,15 @@ BEGIN
         e.TitMainTitle AS title,
         e.CreDateCreated AS 'year',
         (SELECT group_concat(concat_ws(' ', ri1.RigAcknowledgement) order by ri1.irn separator '\n') FROM erightsinfo ri1 WHERE ri1.ObjectIRN = e.irn) AS rights,
-        (SELECT group_concat(concat_ws(' ', pi1.PartiesSummaryData, ' b.', pi1.BioBirthDate, ' - ', pi1.BioDeathDate) order by pi1.PartiesIRN separator '\n') FROM epartiesinfo pi1 WHERE pi1.ObjectIRN = e.irn) AS creators,
+        (SELECT group_concat(concat_ws(' ', pi1.PartiesSummaryData, ' (', pi1.CreRole, ') ', ' b.', pi1.BioBirthDate, ' - ', pi1.BioDeathDate) order by pi1.PartiesIRN separator '\n') FROM epartiesinfo pi1 WHERE pi1.ObjectIRN = e.irn) AS creators,
         'place_created' AS place_created,
         CONCAT(e.PhyVerbatimMeasurementsA, ' ', e.PhyVerbatimMeasurementsB) AS dimensions,
         e.AccCreditLineLocal AS credit,
         CONCAT(e.PhyMediumText, ' ', e.PhySupport) AS media_statement,
         'digital fields' AS special_digital_meta,
-        (SELECT group_concat(concat_ws(' ', e.TitMainTitle, ' ', e.CreDateCreated, ' ', pi2.PartiesSummaryData, ' b.', pi2.BioBirthDate, ' - ', pi2.BioDeathDate) order by pi2.PartiesIRN separator '\n') FROM epartiesinfo pi2 WHERE pi2.ObjectIRN = e.irn) AS short_caption,
+        (SELECT group_concat(concat_ws(' ', e.TitMainTitle, ' ', e.CreDateCreated, ' ', pi2.PartiesSummaryData, ' (', pi2.CreRole, ') ', ' b.', pi2.BioBirthDate, ' - ', pi2.BioDeathDate) order by pi2.PartiesIRN separator '\n') FROM epartiesinfo pi2 WHERE pi2.ObjectIRN = e.irn) AS short_caption,
         CONCAT_WS(' ',
-            (SELECT group_concat(concat_ws(' ', pi3.PartiesSummaryData, CHAR(13),NULLIF(pi3.BioCulturalIdentity1, ''),pi3.Country, ' ',  ' b.', pi3.BioBirthDate, ' - ', pi3.BioDeathDate) order by pi3.PartiesIRN separator '\n')
+            (SELECT group_concat(concat_ws(' ', pi3.PartiesSummaryData, ' (', pi3.CreRole, ') ', CHAR(13),NULLIF(pi3.BioCulturalIdentity1, ''),pi3.Country, ' ',  ' b.', pi3.BioBirthDate, ' - ', pi3.BioDeathDate) order by pi3.PartiesIRN separator '\n')
             FROM epartiesinfo pi3
             WHERE pi3.ObjectIRN = e.irn),
                 NULLIF(e.TitMainTitle, ''), ' ', NULLIF(e.CreDateCreated, ''), CHAR(13),
@@ -73,9 +74,9 @@ BEGIN
             ) AS long_caption,
         DATE(NOW()) AS last_updated
     FROM ecatalogue e
-        LEFT JOIN Objects_attachedCreatorIRNS_csv objirn ON objirn.ObjectIRN = e.irn
+        LEFT JOIN Objects_attachedCreatorIRNS objirn ON objirn.ObjectIRN = e.irn
         LEFT JOIN eparties cre ON objirn.CreatorIRN = cre.PartiesIRN
-        LEFT JOIN Objects_attachedRightsIRNS_csv rigirn ON rigirn.ObjectIRN = e.irn
+        LEFT JOIN Objects_attachedRightsIRNS rigirn ON rigirn.ObjectIRN = e.irn
         LEFT JOIN erights r ON rigirn.RightsIRN = r.irn;
 End
 //
